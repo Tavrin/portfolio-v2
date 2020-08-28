cd40e509de5bd5a1b0269dae8dcd778040ee33ac
etienne <etienne.doux@protonmail.com>
2020-08-28 02:37:44 +0200

    quizzymaths

---
+++ dist/quizzymaths/js/perso.js
@@ -0,0 +1,9 @@
+$( "#target1" ).click(function() {
+  var rep = $("searchFieldText1").(val);
+  if (rep = 10) {
+    alert( "Handler for .click() called." );
+  }
+else {
+  alert("Ce n'est pas la bonne réponse, essaye encore");
+}
+});
