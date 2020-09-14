2478b7285d9cd8c6970d39948a04fa0e6310f330
etienne <etienne.doux@protonmail.com>
2020-09-14 19:26:19 +0200

    réparation textures étoiles

--- src/index.js
+++ src/index.js
@@ -60,8 +60,8 @@ function init() {
 
     var textureLoader = new THREE.TextureLoader();
     // const textureSun =textureLoader.load('/public/textures/2k_sun.jpg')
-    const textureStar1 = textureLoader.load('/dist/public/textures/2k_sun2.jpg')
-    var sprite1 = textureLoader.load( '/dist/public/textures/sprite1.png' );
+    const textureStar1 = textureLoader.load('/public/textures/2k_sun2.jpg')
+    var sprite1 = textureLoader.load( '/public/textures/sprite1.png' );
     // var meteorMat = textureLoader.load( '/public/textures/4k_ceres_fictional.jpg' );
     // var meteorMat2 = textureLoader.load( '/public/textures/8d12c1eb21eb70291bb884ae4f8984dc.png' );
     renderer = new THREE.WebGLRenderer({antialias: true});
